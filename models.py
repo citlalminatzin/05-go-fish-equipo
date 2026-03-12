6011b8742f829d89f22ad4b68b566d0775b5a94d
joyce-bit-202002020 <joyce01herrera@ciencias.unam.mx>
2026-03-12 06:23:40 +0000

    feat: implementa modelo de similitud geométrica y cálculo de constante k

--- models.py
+++ models.py
@@ -9,12 +9,52 @@ que utilices este archivo para que escribas las
 funciones principales que vayas a reutilizar en
 tus otras prácticas
 """
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cal_k_constante(longitudes, pesos):
+    k_valores = pesos / (longitudes**3)
+    return np.mean(k_valores)
+
+def relacion_lineal_modpearson(x, y):
+    relac_lineal = np.corrcoef(x, y)[0,1]
+    return relac_lineal
 
 def modelo_geom(longitudes: list[float]) -> list[float]:
+    pesos_predichos = []
+
+    for L in longitudes:
+        W = K_optimo * (L**3)
+        pesos_predichos.append(W)
+
+    return pesos_predichos
+
+longitud = np.array([36.81, 31.77, 43.82, 36.82, 32.07, 45.07, 35.89])
+peso_campeonato = np.array([0.78, 0.47, 1.16, 0.74, 0.44, 1.40, 0.64])
+
+K_optimo = cal_k_constante(longitud, peso_campeonato)
+
+peso_predicho = modelo_geom(longitud)
+
+r = relacion_lineal_modpearson(longitud**3, peso_campeonato)
+
+plt.figure(figsize=(8,5))
+plt.scatter(longitud, peso_campeonato, color='navy', label='Datos del campeonato')
+
+l_espacio = np.linspace(min(longitud), max(longitud), 100)
+plt.plot(l_espacio, K_optimo * (l_espacio**3), color='mediumpurple', label='Modelo usado $W = Kl^3$')
+
+plt.xlabel('Longitud (cm)')
+plt.ylabel('Peso (kg)')
+plt.title('Primer Modelo de Similitud Geométrica ajustado')
+
+plt.legend()
+plt.grid(True)
+plt.show()
+
     """
-    longitudes: list[float] ¿Qué significa longitudes? 
-    (Por favor elimina la pregunta y reemplazala con su respuesta)
-    ...
+    longitudes: list[float] representa la lista de longitudes (en cm) de los peces se usa para calcular los pesos de los mismos. 
+
     """
     ... # Puedes eliminar esta línea
 
